6c01ab1f6c8293df3cae37dc502a5ac90f9347fc
gaobin <caoww@ect888.com>
2018-12-02 15:54:53 +0800

    脚手架搭建

---
+++ view/components/list.js
@@ -0,0 +1,9 @@
+<script id="list" type="text/html">
+
+  {{# layui.each(d.content, function(index, item){ }}
+
+<h3>{{item.title}}</h3>
+<a href={{item.path}} target="_blank">{{item.decs}}</a>
+{{#  }) }}
+
+</script>
